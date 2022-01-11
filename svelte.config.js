e970c0f057831ab86ee20d726c344c2a387ed303
Stefan Watt <stefan.watt@gmail.com>
2022-01-11 23:10:48 +0100

    updated svelte.config

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,5 @@
 import sveltePreprocess from "svelte-preprocess";
-import adapterStatic from "@sveltejs/adapter-static"
 export default {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-	kit: {
-	adapter: adapterStatic()
-	},
   preprocess: [
     sveltePreprocess({
       postcss: true,
