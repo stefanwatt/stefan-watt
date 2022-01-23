2e5ef3e25aafb3daa1739916ab291e52d48fa431
Stefan Watt <stefan.watt@gmail.com>
2022-01-23 23:14:19 +0100

    updated readme

--- src/lib/components/interests/keyboards.svelte
+++ src/lib/components/interests/keyboards.svelte
@@ -1,11 +1,11 @@
 <div class="card bg-primary-content text-primary">
     <figure>
-      <img class="Sirv" data-options="lazy:false" data-src="https://comentic.sirv.com/Images/iris_rev_2.jpg" alt="iris_rev_2" />
+      <img class="Sirv" data-options="lazy:false" data-src="https://comentic.sirv.com/Images/IMG_20220123_202440.jpg" alt="iris_rev_2" />
     </figure>
     <div class="card-body">
       <h2 class="card-title text-2xl md:text-3xl">Building ergonomic keyboards ⌨️</h2>
       <p class="text-lg md:text-xl mb-4">
-        This is the Iris Rev. 2. It's the first one I've built.
+        This is the DracuLad
       </p>
       <p>
         <strong>
@@ -13,14 +13,15 @@
           </strong>
       </p>
       <ul class="list-disc px-6 font-light">
-        <li>54 keys (Regular Full-Size-Board has about double the keys)</li>
+        <li>36 keys (Regular Full-Size-Board has about triple the keys)</li>
+        <li>split design so your arms can be at shoulder width</li>
         <li>column staggered because your fingers have different lengths</li>
-        <li>tented for more natural wrist angle</li>
-        <li>Micro-USB (to PC) + TRRS (between halves)</li>
+        <li>2 rotary encoders</li>
+        <li>fully wireless thanks to nice!nano micro controller</li>
         <li>Workman Layout because QWERTY just makes no sense at all</li>
       </ul>
       <p class="mt-4">
-        I built this from scratch. The case is 3D-printed. It really has revolutionized the way I think about keyboards. At first it was challenging to live with this only about half the keys. Now I don't even use the top-most row and the outer-most column. So I can actually live with about 36 keys. That means I never have to move my fingers more than one column/row from the home row. All the missing characters are accessed through layers (hold backspace + X/M/V = Number 1/2/3).
+        I built this from scratch. It's the third one I've built. I started out with a 54-key split keyboard and then moved on to this. At first it was challenging to live with this only about half the keys. Then I realized I don't even use the top-most row and the outer-most column. So I can actually live with 36 keys. That means I never have to move my fingers more than one column/row from the home row. All the missing characters are accessed through layers (hold backspace + X/M/V = Number 1/2/3).
       </p>
     </div>
     <div class="flex flex-col w-full">
