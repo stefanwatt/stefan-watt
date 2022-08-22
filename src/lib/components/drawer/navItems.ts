ae6ffeb710433a427743f256263f1fa504bc8522
Stefan Watt <stefan.watt@gmail.com>
2022-08-22 23:54:18 +0200

    updated navbar

--- src/lib/components/drawer/navItems.ts
+++ src/lib/components/drawer/navItems.ts
@@ -1,7 +1,7 @@
 export default [
-  { id: 0, text: 'About', icon:'👨',url:'#about'},
-  {id:1,text:'Projects',icon:'🛠️',url:'#projects'},
-  {id:2,text:'Skills',icon:'💪',url:'#skills'},
-  {id:3,text:'Interests',icon:'❤️',url:'#interests'},
+  { id: 0, text: 'About', icon: '👨', url: '#about' },
+  { id: 1, text: 'Career', icon: '💼', url: '#career' },
+  { id: 2, text: 'Projects', icon: '🛠️', url: '#projects' },
+  { id: 3, text: 'Skills', icon: '💪', url: '#skills' },
+  { id: 4, text: 'Interests', icon: '❤️', url: '#interests' }
 ]
-
